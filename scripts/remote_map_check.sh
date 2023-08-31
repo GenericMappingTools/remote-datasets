0717e2b0e1508e4463f4924f9fcc426b6961f251
Paul Wessel <pwessel@hawaii.edu>
2023-08-31 13:52:44 +0200

    Update check scripts to use candidate server
    Also add a Makefile in addiiton to updating the scirpts.

--- scripts/remote_map_check.sh
+++ scripts/remote_map_check.sh
@@ -1,20 +1,14 @@
 #!/bin/bash -e
-# remote_map_check.sh - Make global test maps for both 05m and 06m resolutions
+# remote_map_check.sh - Make Earth and planetary test maps for both 05m and 06m resolutions
 #
 # usage: remote_map_check
 #
-# Loops over all recipies and makes global maps of them for the two resolutions
-# This checks that the tiled and single grid versions are similar
+# Loops over all recipes and makes global maps of them for the two resolutions
+# This checks that the tiled and single grid versions are similar.  We use the
+# candidate server which must pass all tests before overwriting oceania.
 #
-list=(age day faa gebco gebcosi geoid mag mag4km mask night relief vgg wdmam)
+kind=(earth planets)
 
-for set in ${list[@]}; do
-	gmt begin check_${set} png
-		gmt subplot begin 2x1 -Fs15c/12c -Rg -JH15c -A -T"earth_${set}"
-		gmt subplot set 0 -A"6m"
-		gmt grdimage @earth_${set}_06m -Rg
-		gmt subplot set 1 -A"5m"
-		gmt grdimage @earth_${set}_05m -Rg
-		gmt subplot end
-	gmt end show
+for set in ${kind[@]}; do
+	remote_map_${set}_check
 done
